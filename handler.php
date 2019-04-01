3384f6fdbcceacac91f6e901bdbdb1748a86c706
C.C.Tishchenko <constish@gmail.com>
2019-04-02 00:25:54 +0300

    test

--- handler.php
+++ handler.php
@@ -5,7 +5,7 @@ include 'manage.php';
 include 'sys/system.php';
 
  $preload_text = bot_manager($message, $chat_id);	
-//---------------------------------------------
+//---------------------------------------------K
   sendMessage($chat_id, $preload_text);
 ?>
 
