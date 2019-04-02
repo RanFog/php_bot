8b69e717568d1abdba8a4384f0dbf7cb3637eb73
C.C.Tishchenko <constish@gmail.com>
2019-04-02 14:12:08 +0300

    testtesttest

--- manage.php
+++ manage.php
@@ -9,10 +9,10 @@ function bot_manager($message, $chat_id)
 	//Понижение регистра
 	$mess = strtolower($message);
 	//Менеджер
-	if (substr($mess,1,4) == "head") sendMessage($chat_id, "Староста: Володя Скрылев\n @Vladimir_Skrylev\n +79856142019");
-	if ($mess == "староста") sendMessage($chat_id, "Староста: Володя Скрылев\n @Vladimir_Skrylev\n +79856142019");
-	if ($message == "Староста") sendMessage($chat_id, "Староста: Володя Скрылев\n @Vladimir_Skrylev\n +79856142019");
-	if ($mess == "starosta") sendMessage($chat_id, "Староста: Володя Скрылев\n @Vladimir_Skrylev\n +79856142019");
+	//if (substr($mess,1,4) == "head") sendMessage($chat_id, "Староста: Володя Скрылев\n @Vladimir_Skrylev\n +79856142019");
+	//if ($mess == "староста") sendMessage($chat_id, "Староста: Володя Скрылев\n @Vladimir_Skrylev\n +79856142019");
+	//if ($message == "Староста") sendMessage($chat_id, "Староста: Володя Скрылев\n @Vladimir_Skrylev\n +79856142019");
+	//if ($mess == "starosta") sendMessage($chat_id, "Староста: Володя Скрылев\n @Vladimir_Skrylev\n +79856142019");
 	if ($mess[0] == "/")
 		{
 		//----- Обязательные команды -- help и start
@@ -25,7 +25,7 @@ function bot_manager($message, $chat_id)
 		if (substr($mess,1,4) == "rasp") raspisan($chat_id);
 		//----- Команды помошники -----
 		if (substr($mess,1,7) == "chat_id") sendMessage($chat_id, $chat_id);
-		if (substr($mess,1,9) == "headgroup") sendMessage($id_chat, $chat_id);
+		//if (substr($mess,1,9) == "headgroup") sendMessage($id_chat, $chat_id);
 		//if (substr($mess,1,8) == "староста") sendMessage($id_chat, "Володя Скрылев, @Vladimir_Skrylev +7 (985) 614-20-19");
 		}
 	}
