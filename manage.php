1ec2f1196d1d3b7afbdb92898221e92cbfe13c47
C.C.Tishchenko <constish@gmail.com>
2019-04-04 19:26:26 +0300

    Auth_3

--- manage.php
+++ manage.php
@@ -1,34 +1,53 @@
-//Файл менеджер - распознает команды и запускает сооветсвующие скрипты
 <?php
+//Файл менеджер - распознает команды и запускает сооветсвующие скрипты
 include 'Auth/Auth.php';
 include 'tfcommands/text.php'; //Файл с текстами команд
 include 'UO/UO.php';           //Команды умного одногруппника
 //include 'ip.php';
 
 function bot_manager($message, $chat_id, $user_id)
-        {
+    {
 	//Понижение регистра
 	$mess = strtolower($message);
-	//Менеджер
-	//if (substr($mess,1,4) == "head") sendMessage($chat_id, "Староста: Володя Скрылев\n @Vladimir_Skrylev\n +79856142019");
-	//if ($mess == "староста") sendMessage($chat_id, "Староста: Володя Скрылев\n @Vladimir_Skrylev\n +79856142019");
-	//if ($message == "Староста") sendMessage($chat_id, "Староста: Володя Скрылев\n @Vladimir_Skrylev\n +79856142019");
-	//if ($mess == "starosta") sendMessage($chat_id, "Староста: Володя Скрылев\n @Vladimir_Skrylev\n +79856142019");
-	if ($mess[0] == "/")
+
+	switch (Auth($user_id))
 		{
-		//----- Обязательные команды -- help и start
-        if (substr($mess,1,4) == "help") return TBHELP;
-        if (substr($mess,1,5) == "start") return TBSTART;
-		//----- CCNA_BOT ----
-		if (substr($mess,1,5) == "proxy") return TBPROXY;
-		if (substr($mess,1,3) == "ip") return TBIPI;
-		//----- Умный одногруппник -----
-		if (substr($mess,1,4) == "rasp") raspisan($chat_id);
-		//----- Команды помошники -----
-		if (substr($mess,1,7) == "chat_id") sendMessage($chat_id, $chat_id);
-		if (substr($mess,1,7) == "user_id") sendMessage($chat_id, $user_id);
-		if (substr($mess,1,9) == "headgroup") sendMessage($id_chat, $chat_id);
-		//if (substr($mess,1,8) == "староста") sendMessage($id_chat, "Володя Скрылев, @Vladimir_Skrylev +7 (985) 614-20-19");
+		case 'root':
+			if ($mess[0] == "/")
+				{
+				//----- Обязательные команды -- help и start
+   	 	        if (substr($mess,1,4) == "help") return TBHELP_ROOT;
+ 		        if (substr($mess,1,5) == "start") return TBSTART;
+			    //----- CCNA_BOT ----
+			    if (substr($mess,1,5) == "proxy") return TBPROXY;
+			    if (substr($mess,1,3) == "ip") return TBIPI;
+			    //----- Умный одногруппник -----
+			    if (substr($mess,1,4) == "rasp") raspisan($chat_id);
+			    //----- Команды помошники -----
+			    if (substr($mess,1,7) == "chat_id") sendMessage($chat_id, $chat_id);
+			    if (substr($mess,1,7) == "user_id") sendMessage($chat_id, $user_id);
+				if (substr($mess,1,9) == "headgroup") sendMessage($id_chat, $chat_id);
+				break;
+			    }
+		case 'student':
+			if ($mess[0] == "/")
+				{
+				//----- Обязательные команды -- help и start
+   	     	    if (substr($mess,1,4) == "help") return TBHELP;
+ 	    	    if (substr($mess,1,5) == "start") return TBSTART;
+			    //----- CCNA_BOT ----
+			    if (substr($mess,1,5) == "proxy") return TBPROXY;
+			    if (substr($mess,1,3) == "ip") return TBIPI;
+		    	//----- Умный одногруппник -----
+		    	if (substr($mess,1,4) == "rasp") raspisan($chat_id);
+		  		//----- Команды помошники -----
+		    	if (substr($mess,1,7) == "chat_id") sendMessage($chat_id, $chat_id);
+		   	    if (substr($mess,1,7) == "user_id") sendMessage($chat_id, $user_id);
+		  	    if (substr($mess,1,9) == "headgroup") sendMessage($id_chat, $chat_id);
+				//if (substr($mess,1,8) == "староста") sendMessage($id_chat, "Володя Скрылев, @Vladimir_Skrylev +7 (985) 614-20-19");
+				break;
+				}
+		default: break;
 		}
 	}
 ?>
