44f4b9065daf0f631c6cf393dcf0edfec5d6b286
C.C.Tishchenko <constish@gmail.com>
2019-04-08 19:22:05 +0300

    add_logs_2

---
+++ log.php
@@ -0,0 +1,11 @@
+<?php
+
+function wr_json_to_logs()
+    {
+    $file = fopen('logs/input_logs', wr);
+    fwrite($file, $output);
+    fclose($file);
+    sendMessage($chat_id, "Записано");
+    }
+
+?>
