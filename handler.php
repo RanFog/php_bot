6c3c7be90c52dcf0b5bb5ae9222936a3b4792cad
C.C.Tishchenko <constish@gmail.com>
2019-04-08 19:29:18 +0300

    add_logs_5

--- handler.php
+++ handler.php
@@ -4,9 +4,9 @@
 include 'settings.php'; //Добавить файл с токеном бота
 include 'manage.php';   //Главный менеджер - ищет команду в сообщении
 include 'sys/system.php'; //Подготавливает ответ и раздербанивает json
-include 'logs/log.php';   //Ведение лога
+include 'log.php';   //Ведение лога
 
- wr_json_to_logs();
+ //wr_json_to_logs();
  $preload_text = bot_manager($message, $chat_id, $user_id);	
 //---------------------------------------------K
  sendMessage($chat_id, $preload_text);
