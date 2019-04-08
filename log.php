dfb25e3a5c540abc8f9650963b79eef0acdf7758
C.C.Tishchenko <constish@gmail.com>
2019-04-08 19:51:06 +0300

    add_logs_13

--- log.php
+++ log.php
@@ -3,8 +3,8 @@
 function wr_json_to_logs($chat_id, $username, $user_id, $message)
     {
     $lfile = fopen('logs/input_logs', 'wr');
-    fputs($lfile, date("d/m/Y H:i:s  ")."Chat:".$chat_id." Nick:".$username." User".$user_id." Message:".$message."\n");
     fseek($fd, 0, SEEK_END);
+    fputs($lfile, date("d/m/Y H:i:s  ")."Chat:".$chat_id." Nick:".$username." User".$user_id." Message:".$message."\n");
     fclose($lfile);
     //sendMessage($chat_id, "Записано");
     }
