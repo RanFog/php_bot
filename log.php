b7794e3241a7867f881737a2e0720e5cbd9aa46c
C.C.Tishchenko <constish@gmail.com>
2019-04-08 19:27:01 +0300

    add_logs_3

--- log.php
+++ log.php
@@ -2,7 +2,7 @@
 
 function wr_json_to_logs()
     {
-    $file = fopen('logs/input_logs', wr);
+    $file = fopen('logs/input_logs', 'wr');
     fwrite($file, $output);
     fclose($file);
     sendMessage($chat_id, "Записано");
