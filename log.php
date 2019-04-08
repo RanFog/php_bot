7588bbbdca1c74658a51bd715e637c7e70fe1d93
C.C.Tishchenko <constish@gmail.com>
2019-04-08 20:48:50 +0300

    add_logs_23

--- log.php
+++ log.php
@@ -2,19 +2,19 @@
 
 function wr_input_mess_to_logs($chat_id, $username, $user_id, $message)
     {
-    $lfile = fopen('logs/input_logs', 'a');
+    $lfile = fopen('logs/logs', 'a');
     //fseek($lfile, 0, SEEK_END);
     //fputs($lfile, date("d/m/Y H:i:s  ")."Chat:".$chat_id." Nick:".$username." User".$user_id." Message:".$message."\n");
-    fwrite($lfile, "i ".date("d/m/Y H:i:s  ")."Chat:".$chat_id." Nick:".$username." User:".$user_id." Message:".$message."\n");
+    fwrite($lfile, "i ".date("d/m/Y H:i:s  ")."Chat:".$chat_id." Nick:".$username." User:".$user_id." Message:\n".$message."\n");
     fclose($lfile);
     //sendMessage($chat_id, "Записано");
     }
 function wr_output_mess_to_logs($chat_id, $message)
     {
-    $lfile = fopen('logs/input_logs', 'a');
+    $lfile = fopen('logs/logs', 'a');
     //fseek($lfile, 0, SEEK_END);
     //fputs($lfile, date("d/m/Y H:i:s  ")."Chat:".$chat_id." Nick:".$username." User".$user_id." Message:".$message."\n");
-    fwrite($lfile, "o ".date("d/m/Y H:i:s  ")."Chat:".$chat_id." Nick:".NAME_BOT." Message:".$message."\n");
+    fwrite($lfile, "o ".date("d/m/Y H:i:s  ")."Chat:".$chat_id." Nick:".NAME_BOT." Message:\n".$message."\n");
     fclose($lfile);
     //sendMessage($chat_id, "Записано");
     }
