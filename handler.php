57eeadf2e3e68f619e52296b169a6cfff3cad42e
C.C.Tishchenko <constish@gmail.com>
2019-04-08 19:32:11 +0300

    add_logs_6

--- handler.php
+++ handler.php
@@ -1,12 +1,12 @@
 
 <?php
 //Главный скрипт - вызывается при поступлении сообщения от телеги
-include 'settings.php'; //Добавить файл с токеном бота
-include 'manage.php';   //Главный менеджер - ищет команду в сообщении
+include 'settings.php';   //Добавить файл с токеном бота
+include 'manage.php';     //Главный менеджер - ищет команду в сообщении
 include 'sys/system.php'; //Подготавливает ответ и раздербанивает json
-include 'log.php';   //Ведение лога
+include 'log.php';        //Ведение лога
 
- //wr_json_to_logs();
+ wr_json_to_logs();
  $preload_text = bot_manager($message, $chat_id, $user_id);	
 //---------------------------------------------K
  sendMessage($chat_id, $preload_text);
--- log.php
+++ log.php
@@ -3,7 +3,7 @@
 function wr_json_to_logs()
     {
     $lfile = fopen('logs/input_logs', 'wr');
-    fwrite($lfile, "asa");
+    fputs($lfile, "asa");
     fclose($lfile);
     //sendMessage($chat_id, "Записано");
     }
